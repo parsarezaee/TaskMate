32fe7ce5dd294e4a868e3c21f9b79f5004151daf
parsarezaee <me.parsarezaei@gmail.com>
2024-10-25 16:31:41 +0330

    feat: add users app and add users app with custom user model (email as username)

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = "users.CustomUser"
