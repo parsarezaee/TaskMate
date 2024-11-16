ecc865301f23aa2b62586a0f42550ed63599949a
parsarezaee <me.parsarezaei@gmail.com>
2024-11-16 21:26:37 +0330

    ci canceled

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.getenv('DEBUG'))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
